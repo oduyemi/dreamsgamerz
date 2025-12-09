147c609c866cc027b5c329f68dfc4d32323496f7
Yemi Oduyemi <yhermii@gmail.com>
2025-12-09 21:07:08 +0100

    updated game endpoint

--- src/pages/Competition.tsx
+++ src/pages/Competition.tsx
@@ -1,29 +1,23 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { GamersCompetitionTab } from '../components/GamersCompetitionTab';
 import { AppLayout } from '../components/AppLayout';
 import { useMediaQuery, useTheme } from '@mui/material';
 
-const Competition: React.FC = () => {
+const Competition = () => {
   const theme = useTheme();
   const isSmall = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
     <AppLayout>
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle style={{ color: '#caa84c', textAlign: 'center', fontWeight: 700, fontSize: isSmall ? '1.2rem' : '1.5rem' }}>Games</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-          <IonHeader collapse="condense">
-            <IonToolbar>
-              <IonTitle size="large">Tab 2</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          <GamersCompetitionTab />
-        </IonContent>
-      </IonPage>
-      </AppLayout>
+
+      <div className="header">
+        <h1 style={{ fontSize: isSmall ? '1.2rem' : '1.5rem' }}>Games</h1>
+      </div>
+
+      <div className="page-content">
+        <GamersCompetitionTab />
+      </div>
+
+    </AppLayout>
   );
 };
 
---
+++ src/pages/dist/Competition.js
@@ -0,0 +1,15 @@
+"use strict";
+exports.__esModule = true;
+var GamersCompetitionTab_1 = require("../components/GamersCompetitionTab");
+var AppLayout_1 = require("../components/AppLayout");
+var material_1 = require("@mui/material");
+var Competition = function () {
+    var theme = material_1.useTheme();
+    var isSmall = material_1.useMediaQuery(theme.breakpoints.down('sm'));
+    return (React.createElement(AppLayout_1.AppLayout, null,
+        React.createElement("div", { className: "header" },
+            React.createElement("h1", { style: { fontSize: isSmall ? '1.2rem' : '1.5rem' } }, "Games")),
+        React.createElement("div", { className: "page-content" },
+            React.createElement(GamersCompetitionTab_1.GamersCompetitionTab, null))));
+};
+exports["default"] = Competition;
