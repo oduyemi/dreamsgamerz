c212cc3cde725647c572211c6f7bc2d5cee78b42
Yemi Oduyemi <yhermii@gmail.com>
2026-02-13 16:49:56 +0100

    updated

--- src/pages/dist/MovingPicture.js
+++ src/pages/dist/MovingPicture.js
@@ -1,9 +1,8 @@
 "use strict";
 exports.__esModule = true;
 var AppLayout_1 = require("../components/AppLayout");
-var MovingPictureGame_1 = require("../components/MovingPictureGame");
 var MovingPicture = function () {
     return (React.createElement(AppLayout_1.AppLayout, null,
-        React.createElement(MovingPictureGame_1.MovingPictureGameTab, null)));
+        React.createElement(GameArena, null)));
 };
 exports["default"] = MovingPicture;
